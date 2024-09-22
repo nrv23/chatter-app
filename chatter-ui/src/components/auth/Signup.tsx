03efa27f2bb312fac2cf0b5662d278b8e670f792
nrv2391 <navemen23@hotmail.com>
2024-09-22 01:25:49 -0600

    agregar mensaje de error en el registro

--- chatter-ui/src/components/auth/Signup.tsx
+++ chatter-ui/src/components/auth/Signup.tsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
 import { useCreateUser } from "../../hooks/useCreateUser";
+import { useState } from "react";
 
 const Signup = () => {
 
   const [createUser] = useCreateUser();
+  const [error, setError] = useState("")
   return (
     <Auth submitLabel="Signup" onSubmit={async ({email, password}) => {
       try {
@@ -19,7 +21,8 @@ const Signup = () => {
           }
         })
       } catch (error) {
-        console.log({error})
+        console.log({error});
+        throw error;
       }
     }}>
       <Link to={"/login"} style={{ alignSelf: "center" }}>
